4107ea45e474d8dd84dbeca3e93d579009769256
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-18 16:52:33 +0200

    gitignore env

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  cloudinary: {
+    cloudName: 'ddaxulsi7',
+    apiKey: '865594179484123',
+    apiSecret: 'KZWE3wfNuuOamQNyjWTiJetYLaA',
+  }
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  cloudinary: {
+    cloudName: 'ddaxulsi7',
+    apiKey: '865594179484123',
+    apiSecret: 'KZWE3wfNuuOamQNyjWTiJetYLaA' 
+  }
+};
