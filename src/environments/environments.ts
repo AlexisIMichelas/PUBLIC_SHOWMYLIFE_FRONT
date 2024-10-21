b3f3dd8b70c3216c17ed7d89fa99b819e7c3f818
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-21 10:55:14 +0200

    change cloudinary key

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   cloudinary: {
     cloudName: 'ddaxulsi7',
-    apiKey: '865594179484123',
-    apiSecret: 'KZWE3wfNuuOamQNyjWTiJetYLaA' 
+    apiKey: '774926812947897',
+    apiSecret: 'lM82vvfwcA5I8fTCrxdnNzl1pB8'
   }
 };
